92d580a0bd40aaf88d4b213ce13d71f43af2a4b9
Shaharyar Choudhry <77368134+Choudhry-Shaharyar@users.noreply.github.com>
2022-01-14 01:19:12 -0500

    Create Loading.js

---
+++ client/src/components/Loading.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// Credit: John Smilga
+const Loading = () => {
+ return (
+  <div className="loader">
+  </div>
+ )
+}
+
+export default Loading
+
+const Loading = () => {
+ return (
+  <div className="loader">
+  </div>
+ )
+}
+
+export default Loading
