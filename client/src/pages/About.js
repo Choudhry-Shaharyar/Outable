eea43fbc6d798c9921f358be2a2272af1a498c70
Shaharyar Choudhry <77368134+Choudhry-Shaharyar@users.noreply.github.com>
2022-01-14 01:21:34 -0500

    Create About.js

---
+++ client/src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className = "section about-section">
+      <h2>About page</h2>
+      <p>
+      COVID-19 has been the reason for multiple changes and regulations in Ontario.
+      Recently it has been announced that Ontario is set to move into step 2 of its reopeinig plan. One change that this has brought is that indoor dining has been banned. On the other hand outdoor dining with up to 6 people per table is allowed. The purpose of this website is to inform users which restaurants around them have outdoor dining areas. 
+      Not all restaurants have outdoor seeting areas therefore you can use this website in order to see which ones do.</p>
+    </section>
+  )
+}
+
+export default About
